a54a1321b5a74b8b2c6a7a4de3c60aeff7e02f31
pengovn <61360444+pengovn@users.noreply.github.com>
2020-07-19 17:08:38 +0200

    tp correction

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def update(dt):
     space.step(dt * space.speed)
     for el in elements:
         pos = el[0].position
-        if pos[0] < 0:
+        if pos[0] < -10:
            pos[0] = WINDOW_SIZE[0]
-        elif pos[0] > WINDOW_SIZE[0]:
+        elif pos[0] > WINDOW_SIZE[0]+10:
             pos[0] = 0
-        if pos[1] < 0:
+        if pos[1] < -10:
             pos[1] = WINDOW_SIZE[1]
-        elif pos[1] > WINDOW_SIZE[1]:
+        elif pos[1] > WINDOW_SIZE[1] + 10:
              pos[1] = 0
         el[0].position = pos
 
@@ -28,12 +28,12 @@ if __name__ == "__main__":
 
     elements = []
     speed_max = 250
-    for i in range(20):
+    for i in range(50):
         mass = random.randint(1, 5)
         charge = random.randint(-1, 1)
         position = random.randint(10, WINDOW_SIZE[0] - 10), random.randint(10, WINDOW_SIZE[1] - 10)
         velocity = random.randint(-speed_max, speed_max), random.randint(-speed_max, speed_max)
-        radius = mass + 5
+        radius = mass + 5 + random.randint(-2, 2)
         color = random.randint(16, 255), random.randint(16, 255), random.randint(16, 255)
         particule = create_particule(mass, charge, position, velocity, radius)
         particule[1].color = color
