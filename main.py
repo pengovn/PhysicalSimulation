039736bbb79a26f664bac7d895776a214b99e083
pengovn <61360444+pengovn@users.noreply.github.com>
2020-07-20 21:34:55 +0200

    correction tp gravity beging

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pymunk
 import pyglet
 from window import Window
-from physics import (Space, create_particule,
+from physics import (Space, create_particule, apply_gravity,
                      create_electron, create_proton, create_neutron)
 import random
 from constant import WINDOW_SIZE
@@ -9,15 +9,16 @@ from constant import WINDOW_SIZE
 
 def update(dt):
     space.step(dt * space.speed)
+    apply_gravity(elements, dt * space.speed)
     for el in elements:
         pos = el[0].position
-        if pos[0] < -10:
+        if pos[0] < 0:
            pos[0] = WINDOW_SIZE[0]
-        elif pos[0] > WINDOW_SIZE[0]+10:
+        elif pos[0] > WINDOW_SIZE[0]:
             pos[0] = 0
-        if pos[1] < -10:
+        if pos[1] < 0:
             pos[1] = WINDOW_SIZE[1]
-        elif pos[1] > WINDOW_SIZE[1] + 10:
+        elif pos[1] > WINDOW_SIZE[1]:
              pos[1] = 0
         el[0].position = pos
 
@@ -27,13 +28,13 @@ if __name__ == "__main__":
     body = pymunk.Body()
 
     elements = []
-    speed_max = 250
-    for i in range(50):
+    speed_max = 0
+    for i in range(2):
         mass = random.randint(1, 5)
         charge = random.randint(-1, 1)
-        position = random.randint(10, WINDOW_SIZE[0] - 10), random.randint(10, WINDOW_SIZE[1] - 10)
+        position = random.randint(0, WINDOW_SIZE[0]), random.randint(0, WINDOW_SIZE[1])
         velocity = random.randint(-speed_max, speed_max), random.randint(-speed_max, speed_max)
-        radius = mass + 5 + random.randint(-2, 2)
+        radius = mass + 7 + random.randint(-3, 3)
         color = random.randint(16, 255), random.randint(16, 255), random.randint(16, 255)
         particule = create_particule(mass, charge, position, velocity, radius)
         particule[1].color = color
--- physics.py
+++ physics.py
@@ -1,5 +1,6 @@
 import pymunk
-from constant import (GRAVITY, WINDOW_SIZE, 
+from pymunk.vec2d import Vec2d
+from constant import (GRAVITY, WINDOW_SIZE, GRAVITATIONAL_CONSTANT,
                       ELECTRON_CHARGE, PROTON_CHARGE, NEURTON_CHARGE,
                       ELECTRON_MASS, PROTON_MASS, NEUTRON_MASS)
 import pymunkoptions
@@ -38,3 +39,20 @@ def create_neutron(position: tuple, velocity: tuple, radius: float) -> (pymunk.B
     particule = create_particule(NEUTRON_MASS, NEUTRON_CHARGE, position, velocity, radius)
     particule[1].color = 0, 255, 0
     return particule
+
+
+def apply_gravity(elements: list, dt):
+    list_vec = [Vec2d() for i in range(len(elements))]
+    for i, current_element in enumerate(elements):
+        for j, element in enumerate(elements[i+1:]):
+            el_pos = element[0].position
+            if abs(current_element[0].position.x - el_pos.x) > WINDOW_SIZE[0]/2:
+                el_pos.x *= -1
+            if abs(current_element[0].position.y - el_pos.y) > WINDOW_SIZE[1]/2:
+                el_pos.y *= -1
+            d2 = current_element[0].position.get_dist_sqrd(el_pos)
+            f = GRAVITATIONAL_CONSTANT * (current_element[0].mass * element[0].mass) / d2
+            acc = Vec2d(current_element[0].position.x - el_pos.x, current_element[0].position.y - el_pos.y).normalized() * f
+            current_element[0].velocity -= acc * dt
+            element[0].velocity += acc * dt
+            print(acc, end="\t\t\r")
